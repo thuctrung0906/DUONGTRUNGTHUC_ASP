d69fa0b448dba3b3e1d6bc1cdafdb9c122e9ed66
thuctrung0906 <trungthuc.09062004@gmail.com>
2025-03-29 08:34:34 +0700

    first commit

---
+++ DUONGTRUNGTHUC_2122110158/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DUONGTRUNGTHUC_2122110158.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
